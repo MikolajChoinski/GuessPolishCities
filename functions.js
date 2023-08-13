98861d37a15dd928b742ab5d719916df815cae68
Mikolaj Choinski <mikolajchoinski2@gmail.com>
2023-08-14 00:42:59 +0200

    ...

--- functions.js
+++ functions.js
@@ -185,6 +185,21 @@ function getType(entityType, data, ID) {
     }
     return sortedArray;
 }
+
+function sortBiggest(arr) {
+  const sortedArray = arr.slice(); // Create a copy of the input array
+  for (let i = 1; i < sortedArray.length; i++) {
+      const currentElement = sortedArray[i];
+      let j = i - 1;
+      
+      while (j >= 0 && sortedArray[j].Population < currentElement.Population) {
+          sortedArray[j + 1] = sortedArray[j];
+          j--;
+      }
+      sortedArray[j + 1] = currentElement;
+  }
+  return sortedArray;
+}
   
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var guessPop = 0;
